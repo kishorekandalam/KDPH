4953b43f09e5381bfd056ccc1ad0334a9ecc2535
kishorekandalam <105823127+kishorekandalam@users.noreply.github.com>
2024-10-24 11:17:16 -0400

    Update HomeController.cs

--- KDPH Demo/Controllers/HomeController.cs
+++ KDPH Demo/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
 using KDPH_Demo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-/*Added for sync Testing*/
-/*Added for sync Testing from github to devops new*/
-/*Added for sync Testing from github to devops*/
-/*Test auto trigger*/
-/*Test auto trigger github*/
-/*Test auto trigger github2*/
-/*Test auto trigger 2*/
+
 namespace KDPH_Demo.Controllers
 {
     public class HomeController : Controller
